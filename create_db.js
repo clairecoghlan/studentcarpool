ee440ea4117b6af07bb6d7899a8e93352c244fbc
Claire <115326446@umail.ucc.ie>
2018-10-17 12:28:01 +0100

    Update create_db.js
    DB Creation

---
+++ create_db.js
@@ -0,0 +1,19 @@
+/** connect to mySQL and create DB called mydb
+*/
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.query("CREATE DATABASE mydb", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
+});
